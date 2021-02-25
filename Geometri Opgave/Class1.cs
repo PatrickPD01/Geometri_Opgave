75c9bcd06dbcfe064a14e262d519e2b4b983017a
PatrickPD01 <61233451+PatrickPD01@users.noreply.github.com>
2021-02-25 09:38:14 +0100

    Add files via upload

---
+++ Geometri Opgave/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometri_Opgave
+{
+    public class Square
+    {
+        public int a;
+        private double areaOfSquare;
+        private double circumferenceOfSquare;
+
+        public Square(int a)
+        {
+            // Creating an constructor which takes a as perimeter 
+            this.a = a;
+            int s, perimeter;
+
+            // Prints an informative message, which invites the user to enter the side of the square a
+            Console.Write("Side : ");
+
+            // Reads an integer from the console.
+            s = Convert.ToInt32(Console.ReadLine());
+
+            // Keeps in a new variable area the result of the multiplication of a by a.
+            perimeter = s * s;
+            Console.WriteLine("Perimeter of Square : " + perimeter);
+        }
+
+
+    }
+}
---
+++ Geometri Opgave/Geometri Opgave/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Geometri_Opgave
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Makes it possible for you to insert a specific length of the square
+            Console.WriteLine("Enter length of square:");
+            double squareLength = Convert.ToDouble(Console.ReadLine());
+
+            // Calculates the area of the square
+            double areaOfSquare = squareLength * squareLength;
+            Console.WriteLine("Area of square is: " + areaOfSquare);
+
+
+            // Calculates the circumference of the square
+            double circumferenceOfSquare = squareLength * squareLength;
+            Console.WriteLine("Circumference of square is: " + circumferenceOfSquare);
+        }
+    }
+}
